10dd38634927c351d089890c2d3e017a0820a5cf
abubakarwd <abkk222002@gmail.com>
2025-07-03 23:27:24 +0500

    arrays

---
+++ Day 3/arrays.js
@@ -0,0 +1,37 @@
+// array
+var city = ["Peshawar", "Charsadda", "Mardan"]
+document.write(city);
+
+document.write("<br>");
+
+// declare an array and then add elements
+
+var pets = [];
+pets[0] = "Cat";
+pets[1] = "Dog";
+pets[2] = "Fish";
+pets[3] = "Mouse";
+pets[4] = "Lion";
+document.write(pets);
+
+document.write("<br>");
+
+// replace elements in an already declared array
+pets[0] = "Birds";
+pets[2] = "Snake";
+pets[4] = "Shark";
+document.write(pets);
+
+var fruits = ["Apple", "Banana", "Grapes"]
+document.write(fruits);
+
+document.write("<br>");
+
+fruits.pop();               // the (pop) keyword/function removes the last element from the array
+document.write(fruits);
+
+document.write("<br>");
+
+fruits.push("Mango", "Pineapple")      // the (push) keyword/function adds element to the end of array
+document.write(fruits);
+
